baf5545368142bd43f0b29b886c11c4a8ed07683
jimin <jim1286@khu.ac.kr>
2022-09-29 14:38:13 +0900

    Fix Main

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/common/Header';
+import Carousel from '../../components/carousel/Carousel';
+
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <Carousel />
+    </>
+  )
+}
