c1e66f5ca9d2d0925f238652c7218595e91dfacc
jimin <jim1286@khu.ac.kr>
2022-09-29 18:58:26 +0900

    Feat : Change Folder

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
-import Header from '../../components/common/Header';
-import Carousel from '../../components/carousel/Carousel';
-import Footer from '../../components/common/Footer';
-import { PersonalSection } from "../.././components/personalSection/PersonalSection"
+import Header from '../components/common/Header';
+import Carousel from '../components/carousel/Carousel';
+import Footer from '../components/common/Footer';
+import { PersonalSection } from '../components/personalSection/PersonalSection';
 
 const MainPageStyles = styled.div`
   width: 62vw;
