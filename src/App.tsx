07e5829939d5e4308b6d11010e70b6a80083032f
Yupmac <hohobanjang@gmail.com>
2022-09-29 09:00:18 +0900

    feat: create PersonalSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage } from './pages/MainPage';
 import { LoginPage } from './pages/LoginPage';
+import GlobalStyle from './styles/global-styles';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <GlobalStyle />
         <Routes>
           <Route path="/" element={ <MainPage /> } />
           <Route path="/login" element={ <LoginPage /> } />
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,15 @@
-import PersonalSection from "../components/PersonalSection/PersonalSection"
+import styled from 'styled-components';
+import { PersonalSection } from "../components/PersonalSection/PersonalSection"
+
+const MainPageStyles = styled.div`
+  width: 62vw;
+  margin: 0 auto;
+`
 
 export const MainPage = () => {
-  return <div>
-    <PersonalSection />
-  </div>
+  return (
+    <MainPageStyles>
+      <PersonalSection />
+    </MainPageStyles>
+  )
 }
