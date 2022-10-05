11ca7165bcb7ff7bfbf3228c84b87c904bf55fd7
jimin <jim1286@khu.ac.kr>
2022-10-05 19:26:52 +0900

    Feat : 어드민 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import Main from "./pages/main/Main";
-import Error from "./pages/error/Error";
-import { Route, Routes, Navigate } from "react-router-dom";
-import SignUp from "./pages/signUp/SignUpPage";
-import Mypage from "./components/mypage/Mypage";
+import React from 'react';
+import Main from './pages/main/Main';
+import Error from './pages/error/Error';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from './pages/signUp/SignUpPage';
+import Mypage from './components/mypage/Mypage';
+import Admin from './pages/admin/Admin';
+import Admin_Main from './components/admin/Admin_Main';
+import Admin_Product from './components/admin/Admin_Product';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="*" element={<Error />} />
+        {/* <Route path="*" element={<Error />} /> */}
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/mypage" element={<Mypage/>}/>
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/admin/login" element={<Admin />} />
+        <Route path="/admin" element={<Admin_Product />} />
       </Routes>
     </>
   );
