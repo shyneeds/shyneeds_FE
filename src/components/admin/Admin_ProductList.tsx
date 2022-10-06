b82c6b216dad14ce238fbc41c086919fa224e767
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-07 01:08:12 +0900

    Feat : 버튼 디자인, select box 디자인, 게시판 디자인 수정

--- src/components/admin/Admin_ProductList.tsx
+++ src/components/admin/Admin_ProductList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Data } from './Admin_Product';
 
 type userData = {
@@ -6,17 +7,24 @@ type userData = {
 };
 
 export default function Admin_ProductList({ data }: userData) {
-  const { title, summary, price } = data;
+  const { title, summary, price, searchKeyword } = data;
   return (
-    <>
-      <div>
-        <p>상품명</p>
-      </div>
-      <div>
-        <p>{title}</p>
-        <p>{summary}</p>
-        <p>{price}</p>
-      </div>
-    </>
+    <List>
+      <input type="checkbox"></input>
+      <p>{title}</p>
+      <p>{summary}</p>
+      <p>{price}</p>
+      <p>{searchKeyword[0]}</p>
+    </List>
   );
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05),
+    2px 2px 3px 1px rgba(208, 213, 219, 0.28);
+`;
