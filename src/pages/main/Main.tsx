889ec33b1ddbb4e9f81ba5c076b76b6ed60c7c8e
Yupmac <hohobanjang@gmail.com>
2022-10-06 13:34:12 +0900

    개인 작업물

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -4,24 +4,28 @@ import Footer from '../../components/common/Footer';
 import { PersonalSection } from '../../components/personalSection/PersonalSection';
 import TopButton from '../../components/common/TopButton';
 import { KakaoTalkBanner } from '../../components/banner/KakaoTalkBanner';
-import { useState } from "react";
+// import { useState } from 'react';
 import Review from '../../components/review/Review';
 import { PopupBanner } from '../../components/banner/PopupBanner';
-
+import { RecommendedByGroup } from '../../components/recommended/RecommendedByGroup';
+import { RecommendedBanner } from '../../components/recommended/RecommendedBanner';
 
 export default function Main() {
-  const [show, setShow] = useState(true);
+  // const [show, setShow] = useState(true);
 
   return (
     <>
       <Header />
       <Carousel />
       <PersonalSection />
+      <RecommendedBanner />
       <Review />
       <Footer />
-      {show ? <PopupBanner onClose={setShow} /> : null}
+      {/* {show ? <PopupBanner onClose={setShow} /> : null} */}
+      <PopupBanner />
       <KakaoTalkBanner />
       <TopButton />
+      <RecommendedByGroup />
     </>
   );
 }
