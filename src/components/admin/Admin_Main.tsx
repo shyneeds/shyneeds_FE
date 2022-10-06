3806433951109e8d1cfa73022f7229c34e085e4e
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-07 01:10:02 +0900

    Style : 버튼,select box,게시판 디자인 수정

--- src/components/admin/Admin_Main.tsx
+++ src/components/admin/Admin_Main.tsx
@@ -12,7 +12,11 @@ export default function Admin_Main() {
       .then((res) => setDatas(res.data.data));
   }, []);
 
-  console.log(datas);
+  // console.log(datas);
+
+  const addProduct = () => {
+    console.log('test');
+  };
 
   return (
     <Wrap>
@@ -20,18 +24,27 @@ export default function Admin_Main() {
         <img src={process.env.PUBLIC_URL + '/icons/logo-admin.png'} />
       </Header>
       <Nav>
-        <Nav_Bottom></Nav_Bottom>
+        <Nav_Bottom>사이트 바로가기</Nav_Bottom>
       </Nav>
       <Main>
         <Main_Wrap>
-          <Main_Add_Btn
-          // onClick={addProduct}
-          >
+          <Main_Add_Btn onClick={addProduct}>
             <p>상품 추가하기</p>
           </Main_Add_Btn>
+          <Main_List_Number>
+            <option value="5개">5개</option>
+            <option value="10개">10개</option>
+          </Main_List_Number>
           <Main_Board>
             <Main_List>
-              {datas.map((data: any) => (
+              <Main_List_Category>
+                <input type="checkbox"></input>
+                <p>상품명</p>
+                <p>요약</p>
+                <p>가격</p>
+                <p>카테고리</p>
+              </Main_List_Category>
+              {datas.map((data: any, i) => (
                 <Admin_ProductList key={data.id} data={data} />
               ))}
             </Main_List>
@@ -74,7 +87,6 @@ const Nav_Bottom = styled.div`
   align-self: flex-end;
   background: #666666;
 `;
-
 const Main = styled.div`
   grid-area: main;
   background: rgb(0, 0, 0, 0.1);
@@ -85,8 +97,6 @@ const Main = styled.div`
 const Main_Wrap = styled.div`
   width: 97%;
   height: 95%;
-  // left: 310px;
-  // top: 112px;
   background: #ffffff;
   border-radius: 8px;
   display: flex;
@@ -97,9 +107,38 @@ const Main_Add_Btn = styled.button`
   width: 127px;
   height: 40px;
   background: #4286f4;
-  border-radius: 8px;
+
+  margin: 0;
+  padding: 0.5rem 1rem;
+
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+
+  border: none;
+  border-radius: 4px;
+
+  display: inline-block;
+  width: auto;
+
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+  cursor: pointer;
+
+  transition: 0.5s;
   right: 30px;
-  margin: 20px 0 0 20px;
+  top: 20px;
+
+  &:active {
+    background: skyblue;
+    outline: 0;
+  }
+  &:disabled {
+    opacity: 0.5;
+  }
   p {
     font-family: 'Pretendard';
     font-style: normal;
@@ -111,8 +150,44 @@ const Main_Add_Btn = styled.button`
 `;
 const Main_Board = styled.div``;
 const Main_List = styled.div`
-  background-color: red;
-  width: 800px;
-  height: auto;
-  margin: 100px 520px 0 120px;
+  margin: 110px 30px 0 30px;
+`;
+const Main_List_Category = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const Main_List_Number = styled.select`
+  position: absolute;
+  font-family: 'Noto Sansf KR', sans-serif;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 1.5;
+  width: 80px;
+  height: 35px;
+  top: 60px;
+  left: 30px;
+
+  color: #444;
+  background-color: #fff;
+
+  padding: 0.3em 0.7em 0.2em 0.4em;
+
+  border: 1px solid #aaa;
+  border-radius: 0.5em;
+  box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+
+  &:hover {
+    border-color: #888;
+  }
+
+  &:focus {
+    border-color: #aaa;
+    box-shadow: 0 0 1px 2px rgba(59, 153, 252, 0.7);
+    color: #222;
+    outline: none;
+  }
+
+  &:disabled {
+    opacity: 0.5;
+  }
 `;
