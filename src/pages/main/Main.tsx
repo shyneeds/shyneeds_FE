30f6a898482f904fe7671fe697293ebc62859ae6
Yupmac <hohobanjang@gmail.com>
2022-10-08 16:17:48 +0900

    feat: add MainBanner on MainPage

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,18 +1,18 @@
 import Header from '../../components/common/Header';
-import Carousel from '../../components/carousel/Carousel';
 import Footer from '../../components/common/Footer';
 import { PersonalSection } from '../../components/personalSection/PersonalSection';
 import TopButton from '../../components/common/TopButton';
 import { KakaoTalkBanner } from '../../components/banner/KakaoTalkBanner';
 import Review from '../../components/review/Review';
 import { RecommendedBanner } from '../../components/recommended/RecommendedBanner';
+import { MainBanner } from '../../components/banner/MainBanner/MainBanner';
 // import { RecommendedByGroup } from '../../components/recommended/RecommendedByGroup';
 
 export default function Main() {
   return (
     <>
       <Header />
-      <Carousel />
+      <MainBanner />
       <PersonalSection />
       <RecommendedBanner />
       <Review />
