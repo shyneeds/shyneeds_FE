712c28f304226888816b33df0ed6557ff4643f17
Yupmac <hohobanjang@gmail.com>
2022-10-08 17:02:40 +0900

    refactor: prettier 적용

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,38 +1,40 @@
-import React from "react";
-import Slider from "react-slick";
-import styled from "styled-components";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import { LAYOUT } from '../../constants/layout';
 
-function Carousel() {
-  const items:any = ['1','2','3','4','5'];
+const Carousel = () => {
+  const items: any = ['1', '2', '3', '4', '5'];
   return (
     <SliderContainer>
       <StyledSlider {...settings}>
-        {items.map((item:any, i:any) => {
-          return <BorderBox key={i}>
-            <img src={`https://placeimg.com/1000/440/any?rand=${i}`} alt="" />
-          </BorderBox>;
+        {items.map((item: any, i: any) => {
+          return (
+            <BorderBox key={i}>
+              <img src={`https://placeimg.com/1000/440/any?rand=${i}`} alt="" />
+            </BorderBox>
+          );
         })}
       </StyledSlider>
     </SliderContainer>
   );
-}
+};
 
 export default Carousel;
 
 const SliderContainer = styled.div`
   width: ${LAYOUT.SIZE.WIDTH};
-  height : 440px;
+  height: 440px;
   margin: 0 auto;
 `;
 
 const BorderBox = styled.div`
   border: 1px solid red;
-  height : 440px;
-  text-align : center;
+  height: 440px;
+  text-align: center;
 `;
+
 const settings = {
   dots: true,
   infinite: true,
@@ -49,14 +51,14 @@ const StyledSlider = styled(Slider)`
   .slick-prev {
     width: 30px;
     height: 30px;
-    margin-left : 30px;
-    margin-right : 50px;
+    margin-left: 30px;
+    margin-right: 50px;
     z-index: 99999;
     top: 45%;
   }
   .slick-next::before,
   .slick-prev::before {
-  font-size: 50px;
-  color: red;
+    font-size: 50px;
+    color: red;
   }
 `;
