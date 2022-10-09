e7aa7ad153363b291689228a7ecb2921ee28d9e3
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-09 21:49:35 +0900

    Feat : style 적용, img와 updateAt 추가

--- src/components/admin/Admin_ProductList.tsx
+++ src/components/admin/Admin_ProductList.tsx
@@ -7,24 +7,61 @@ type userData = {
 };
 
 export default function Admin_ProductList({ data }: userData) {
-  const { title, summary, price, searchKeyword } = data;
+  const { updatedAt, title, summary, price, searchKeyword, mainImage } = data;
+  const updatedAtKR: any = new Date(updatedAt);
   return (
     <List>
       <input type="checkbox"></input>
-      <p>{title}</p>
-      <p>{summary}</p>
-      <p>{price}</p>
-      <p>{searchKeyword[0]}</p>
+      <Title>
+        <img src={mainImage} alt={title} />
+        <p>{title}</p>
+      </Title>
+      <Summary>{summary}</Summary>
+      <Price>{price}</Price>
+      <SearchKeyword>{searchKeyword}</SearchKeyword>
+      <UpdatedAt>{updatedAt}</UpdatedAt>
     </List>
   );
 }
 
 const List = styled.div`
   display: flex;
-  justify-content: space-around;
   align-items: center;
   width: 100%;
   height: 70px;
   box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05),
     2px 2px 3px 1px rgba(208, 213, 219, 0.28);
+  input {
+    width: 3%;
+  }
+  p {
+    font-size: 13px;
+    text-align: center;
+  }
+`;
+const Title = styled.div`
+  height: 100%;
+  width: 12%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    width: 50px;
+  }
+  img {
+    width: 60px;
+    height: 60px;
+  }
+`;
+const Summary = styled.p`
+  width: 50%;
+`;
+const Price = styled.p`
+  width: 10%;
+`;
+const SearchKeyword = styled.p`
+  width: 10%;
+`;
+const UpdatedAt = styled.p`
+  width: 15%;
 `;
