88467fa89783a626c296c9be16b22b5278264a1d
Yupmac <hohobanjang@gmail.com>
2022-10-10 01:16:47 +0900

    feat: add SideBar on MainPage

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,27 +1,25 @@
 import Header from '../../components/common/Header';
 import { MainBanner } from '../../components/banner/MainBanner/MainBanner';
-import { KakaoTalkBanner } from '../../components/banner/KakaoTalkBanner';
+import { SideBar } from '../../components/banner/sideBar/SideBar';
 import { PersonalSection } from '../../components/personalSection/PersonalSection';
 import { RecommendedByGroup } from '../../components/recommended/RecommendedByGroup';
 import { RecommendedByRegion } from '../../components/recommended/RecommendedByRegion';
 import { RecommendedByTheme } from '../../components/recommended/RecommendedByTheme';
 import Review from '../../components/review/Review';
 import Footer from '../../components/common/Footer';
-import TopButton from '../../components/common/TopButton';
 
 export default function Main() {
   return (
     <>
       <Header />
       <MainBanner />
+      <SideBar />
       <PersonalSection />
       <RecommendedByGroup />
       <RecommendedByRegion />
       <RecommendedByTheme />
       <Review />
       <Footer />
-      <KakaoTalkBanner />
-      <TopButton />
     </>
   );
 }
