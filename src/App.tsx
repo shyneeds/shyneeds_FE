cdcb0095ed0edb201a92781fcbec5e76a69460a7
jimin <jim1286@khu.ac.kr>
2022-10-11 20:18:04 +0900

    Feat : add Route offers

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import LoginResult from './components/testLogin/LoginResult';
 import MyPage from './pages/myPage/MyPage';
 import Kakao from './pages/login/Kakao';
 import Community from './pages/community/Community';
+import Offers from './pages/offers/Offers';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/testLogin" element={<TestLoginPage />} />
         <Route path="/testLoginResult" element={<LoginResult />} />
         <Route path="/community" element={<Community />} />
+        <Route path="/offers" element={<Offers />} />
       </Routes>
     </>
   );
