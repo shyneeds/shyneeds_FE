be40191d54a11e5b66abfb20c9d91f803e3842cb
jimin <jim1286@khu.ac.kr>
2022-10-11 20:22:32 +0900

    Feat : 상세페이지 추가

---
+++ src/pages/offers/Offers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import Offers_Header from '../../components/offers/Offers_Header';
+
+export default function Offers() {
+  return (
+    <>
+      <Header />
+      <Offers_Header />
+      <Footer />
+    </>
+  );
+}
