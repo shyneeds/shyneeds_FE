e0bbe93b21195ef29eb2f7f9840774d5afa6e4f0
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-14 03:46:02 +0900

    Feat : 장바구니 페이지 구현
    Header, Cart_Main, Footer

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cart_Main from '../../components/cart/Cart_Main';
+import Footer from '../../components/common/Footer';
+import Header from '../../components/common/Header';
+
+export default function Cart() {
+  return (
+    <>
+      <Header />
+      <Cart_Main />
+      <Footer />
+    </>
+  );
+}
