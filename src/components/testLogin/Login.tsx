187e0b61246c18c62b272d7a6eeece6a6f839c69
Youngpro95 <mmyoung95@naver.com>
2022-10-13 14:55:45 +0900

    fix : 추후 삭제 예정

--- src/components/testLogin/Login.tsx
+++ src/components/testLogin/Login.tsx
@@ -3,14 +3,17 @@ import { KAKAO_AUTH_URL, REDIRECT_URL } from '../../constants/KAKAO_AUTH_URL';
 const {Kakao} = window;
 
 const Login = () => {
-  useEffect(()=>{
-    Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
-    console.log(Kakao.isInitialized())
-  },[])
+  // useEffect(()=>{
+  //   Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
+  //   console.log(Kakao.isInitialized())
+  // },[])
   return (
-      <a href={KAKAO_AUTH_URL}>
-        <img src={process.env.PUBLIC_URL + '/icons/kakao_login_large_narrow.png'} alt='로그인'/>
-      </a>
+      // <a href={KAKAO_AUTH_URL}>
+      //   <img src={process.env.PUBLIC_URL + '/icons/kakao_login_large_narrow.png'} alt='로그인'/>
+      // </a>
+      <>
+        test
+      </>
   )
 }
 
