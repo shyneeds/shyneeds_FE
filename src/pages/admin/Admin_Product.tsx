cf8f3d982c767930fb242d4ca61f94a798ba4cc9
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-14 23:32:06 +0900

    Feat : 어드민 상품 추가하기 스타일 완성
    세부적인 기능 구현은 아직입니다.

--- src/pages/admin/Admin_Product.tsx
+++ src/pages/admin/Admin_Product.tsx
@@ -23,6 +23,8 @@ const registerInfo: postAdminProductData = {
 
 export default function Admin_Product() {
   const [title, setTitle] = useState<string>('');
+  const [summary, setSummary] = useState<string>('');
+  const [price, setPrice] = useState<string>('0');
   let file1: any = useRef();
   let file2: any = useRef();
 
@@ -74,31 +76,80 @@ export default function Admin_Product() {
       <Header>
         <img src={process.env.PUBLIC_URL + '/icons/logo-admin.png'} />
       </Header>
-      <Main>
-        <form id="form" onSubmit={onSubmit}>
-          <input type="file" id="upload-file" onChange={onChangeImg}></input>
-          <input type="text" onChange={(e) => setTitle(e.target.value)}></input>
-          <input type="file" id="upload-file2" onChange={onChangeImg2}></input>
+      <MainWrap>
+        <Main_Header>
+          <p>상세설명</p>
           <button>저장</button>
-        </form>
-      </Main>
+        </Main_Header>
+        <Admin_Main>
+          <Admin_Main_Preview></Admin_Main_Preview>
+          <Admin_Main_Option>
+            <Admin_Main_Option_Text>상품설정</Admin_Main_Option_Text>
+            <Admin_Main_Option_Wrap>
+              <Admin_Main_Option_Name>
+                <p>상품명</p>
+                <input
+                  placeholder="상품명을 적어주세요."
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                  }}
+                />
+              </Admin_Main_Option_Name>
+              <Admin_Main_Option_Price>
+                <p>판매가</p>
+                <input
+                  value={price}
+                  onChange={(e) => {
+                    setPrice(e.target.value);
+                  }}
+                />
+              </Admin_Main_Option_Price>
+              <Admin_Main_Option_Image>
+                <p>이미지 추가</p>
+                <Admin_Main_Option_Image_Wrap>
+                  <Images_Wrap>
+                    <img
+                      src={process.env.PUBLIC_URL + '/icons/add-img.png'}
+                    ></img>
+                    <p>대표이미지 추가</p>
+                  </Images_Wrap>
+                  <Images_Wrap>
+                    <img
+                      src={process.env.PUBLIC_URL + '/icons/add-img.png'}
+                    ></img>
+                    <p>상세이미지 추가</p>
+                  </Images_Wrap>
+                </Admin_Main_Option_Image_Wrap>
+              </Admin_Main_Option_Image>
+              <Admin_Main_Option_Options>
+                <p>옵션</p>
+                <Admin_Main_Option_Options_Wrap></Admin_Main_Option_Options_Wrap>
+              </Admin_Main_Option_Options>
+              <Admin_Main_Option_Category>
+                <p>카테고리</p>
+              </Admin_Main_Option_Category>
+              <Admin_Main_Option_Summary>
+                <p>요약설명(상품상세)</p>
+                <input
+                  onChange={(e) => setSummary(e.target.value)}
+                  placeholder="상품 상세 요약을 적어주세요."
+                ></input>
+              </Admin_Main_Option_Summary>
+            </Admin_Main_Option_Wrap>
+          </Admin_Main_Option>
+        </Admin_Main>
+      </MainWrap>
     </Wrap>
   );
 }
 
-const Wrap = styled.div`
+const Wrap = styled.section`
   width: 100vw;
   height: 100vh;
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  grid-template-rows: 80px auto;
-  grid-template-areas:
-    'header header'
-    'side main';
 `;
-const Header = styled.div`
-  grid-area: header;
+const Header = styled.section`
   background: black;
+  height: 8%;
   display: flex;
   align-items: center;
   img {
@@ -107,4 +158,203 @@ const Header = styled.div`
     height: 28px;
   }
 `;
-const Main = styled.div``;
+const MainWrap = styled.section`
+  width: 100%;
+  height: 92%;
+`;
+const Main_Header = styled.div`
+  display: flex;
+  align-items: center;
+
+  position: relative;
+  width: 100%;
+  height: 7%;
+  border-bottom: 1px solid rgba(33, 33, 33, 0.15);
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+    left: 30px;
+  }
+  button {
+    width: 80px;
+    height: 40px;
+    position: absolute;
+    background: #4286f4;
+    border-radius: 8px;
+    color: white;
+    right: 30px;
+  }
+`;
+const Admin_Main = styled.section`
+  width: 100%;
+  height: 93%;
+  display: flex;
+  flex-direction: row;
+`;
+const Admin_Main_Preview = styled.section`
+  width: 80%;
+  height: 100%;
+  border-right: 1px solid rgba(33, 33, 33, 0.15);
+`;
+const Admin_Main_Option = styled.section`
+  width: 20%;
+  height: 100%;
+  padding: 20px;
+`;
+const Admin_Main_Option_Text = styled.p`
+  width: 100%;
+  height: 5%;
+`;
+const Admin_Main_Option_Wrap = styled.section`
+  width: 100%;
+  height: 95%;
+`;
+const Admin_Main_Option_Name = styled.section`
+  position: relative;
+  width: 100%;
+  height: 10%;
+  padding: 10px;
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #666666;
+  }
+  input {
+    position: absolute;
+    top: 30px;
+    height: 40px;
+    width: 300px;
+    background: #ffffff;
+    border: 1px solid #eeeeee;
+    border-radius: 10px;
+  }
+`;
+const Admin_Main_Option_Price = styled.section`
+  position: relative;
+  width: 100%;
+  height: 10%;
+  padding: 10px;
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #666666;
+  }
+  input {
+    position: absolute;
+    top: 30px;
+    height: 40px;
+    width: 300px;
+    background: #ffffff;
+    border: 1px solid #eeeeee;
+    border-radius: 10px;
+  }
+`;
+const Admin_Main_Option_Image = styled.section`
+  position: relative;
+  width: 100%;
+  height: 25%;
+  padding: 10px;
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #666666;
+  }
+`;
+const Admin_Main_Option_Image_Wrap = styled.section`
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  top: 30px;
+  width: 300px;
+  height: 75%;
+  background: #ffffff;
+  border: 1px solid #eeeeee;
+  border-radius: 10px;
+`;
+const Images_Wrap = styled.section`
+  position: relative;
+  width: 100px;
+  height: 100px;
+  top: 20px;
+  left: 20px;
+  cursor: pointer;
+  img {
+    position: absolute;
+    width: 70%;
+    height: 70%;
+    left: 10px;
+  }
+  p {
+    bottom: 10px;
+  }
+`;
+const Admin_Main_Option_Options = styled.section`
+  position: relative;
+  width: 100%;
+  height: 15%;
+  padding: 10px;
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #666666;
+  }
+`;
+const Admin_Main_Option_Options_Wrap = styled.section`
+  position: absolute;
+  top: 30px;
+  width: 300px;
+  height: 60%;
+  background: #eeeeee;
+  border: 1px solid #eeeeee;
+  border-radius: 10px;
+`;
+const Admin_Main_Option_Category = styled.section`
+  width: 100%;
+  height: 20%;
+  padding: 10px;
+  border: 1px solid rgba(33, 33, 33, 0.15);
+`;
+const Admin_Main_Option_Summary = styled.section`
+  position: relative;
+  width: 100%;
+  height: 15%;
+  padding: 10px;
+  p {
+    position: absolute;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #666666;
+  }
+  input {
+    position: absolute;
+    top: 30px;
+    height: 100px;
+    width: 300px;
+    background: #ffffff;
+    border: 1px solid #eeeeee;
+    border-radius: 10px;
+  }
+`;
