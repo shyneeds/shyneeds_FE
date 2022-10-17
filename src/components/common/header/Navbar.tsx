8abc9b06c56f2a236054e9e93db7d8c607c0dc5d
Yupmac <hohobanjang@gmail.com>
2022-10-18 02:30:29 +0900

    style: GNB 스타일 2차 수정

--- src/components/common/header/Navbar.tsx
+++ src/components/common/header/Navbar.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { LAYOUT } from '../../../constants/layout';
 import { useState } from 'react';
-import { BsDot } from 'react-icons/bs';
 
 export const Navbar = () => {
   const [groupOpen, setGroupOpen] = useState(false);
@@ -74,19 +73,16 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>동남아시아</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>괌&사이판&하와이</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>호주&뉴질랜드</p>
                               </Link>
@@ -99,13 +95,11 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>인도&주변국</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>중앙아시아</p>
                               </Link>
@@ -119,13 +113,11 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>동남아프리카</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>북아프리카&중동</p>
                               </Link>
@@ -139,13 +131,11 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>코카서스</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>유럽</p>
                               </Link>
@@ -159,13 +149,11 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>중남미</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_Icon" />
                               <Link to="">
                                 <p>북미</p>
                               </Link>
@@ -179,19 +167,16 @@ export const Navbar = () => {
                           </SubLi_first>
                           <SubUl_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>대만</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>중국</p>
                               </Link>
                             </SubLi_second>
                             <SubLi_second>
-                              <BsDot className="dot_icon" />
                               <Link to="">
                                 <p>일본</p>
                               </Link>
@@ -226,45 +211,37 @@ export const Navbar = () => {
 
                         <SubBox>
                           <SubLi_first>
-                            <Link to="">
-                              <p>골프여행</p>
-                            </Link>
+                            <Link to="">골프여행</Link>
                           </SubLi_first>
 
                           <SubBox_second>
                             <SubUl_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>유럽/미국/특수지역</p>
                                 </Link>
                               </SubLi_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>태국/라오스/미얀마</p>
                                 </Link>
                               </SubLi_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>필리핀/인도네시아</p>
                                 </Link>
                               </SubLi_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>베트남/캄보디아</p>
                                 </Link>
                               </SubLi_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>싱가폴/기타동남아</p>
                                 </Link>
                               </SubLi_second>
                               <SubLi_second>
-                                <BsDot className="dot_icon" />
                                 <Link to="">
                                   <p>중국/일본/대만</p>
                                 </Link>
@@ -412,10 +389,12 @@ const SubNav_second = styled.div`
 `;
 
 const SubBox = styled.div`
-  width: 450px;
+  max-width: 107px;
 `;
 
-const SubBox_second = styled.div``;
+const SubBox_second = styled.div`
+  margin: 0 0 0 -8px;
+`;
 
 const SubUl_first = styled.ul`
   height: 100%;
@@ -425,7 +404,7 @@ const SubUl_first = styled.ul`
 `;
 
 const SubUl_first_theme = styled(SubUl_first)`
-  gap: 4rem;
+  /* gap: 4rem; */
 `;
 
 const SubUl_second = styled.ul`
@@ -433,33 +412,48 @@ const SubUl_second = styled.ul`
 `;
 
 const SubLi_first = styled.li`
+  font-weight: 600;
   color: #222222;
-  
-  &::after {
-    &:hover {
-      border-bottom: 2px solid #000000;
-      font-weight: bolder;
-      padding-bottom: 0.2rem;
-      color: #000000;
-      width: 50%;
-}
+
+  &:hover a {
+    border-bottom: 2px solid #000000;
+    font-weight: bolder;
+    padding-bottom: 4px;
+    color: #000000;
+    width: 50%;
   }
 `;
 
 const SubLi_second = styled.li`
   display: flex;
+  min-height: 20px;
   font-size: 14px;
   color: #666666;
-  padding: 0 0 4px 0;
+  padding: 0 0 4px 5px;
   margin-top: 16px;
 
-  &:hover {
+  &:hover p {
     color: #000000;
     font-weight: bolder;
     border-bottom: 1px solid #000000;
   }
-
-  .dot_icon {
-    color: #cccccc;
+  a {
+    position: relative;
+  }
+  a:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 7px;
+    left: -8px;
+    width: 4px;
+    height: 4px;
+    margin: -1px 5px 0 0;
+    background-color: #cccccc;
+    border-radius: 50%;
+    vertical-align: middle;
+  }
+  &:hover a:before {
+    // background-color: #000;
   }
 `;
