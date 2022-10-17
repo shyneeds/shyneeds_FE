412160e9ea47b31ee7d1b1b2faaadfe76829a9b8
Youngpro95 <mmyoung95@naver.com>
2022-10-18 03:45:41 +0900

    feat : pagenation 기능 완성!!!!!! without 라이브러리

--- src/features/page/page.tsx
+++ src/features/page/page.tsx
@@ -6,12 +6,14 @@ export interface pageState {
   testData: [];
   total : number;
   page : number;
+  blockNum : number;
 }
 
 const initialState: pageState = {
   testData: [],
-  total : 321, // 추후 api 받아와서 Update 하면됨
+  total : 351, // 추후 api 받아와서 Update 하면됨
   page : 1,
+  blockNum : 0,
 };
 
 export const page = createSlice({
@@ -27,16 +29,27 @@ export const page = createSlice({
       state.page = action.payload;
       console.log("page : " +state.page)
     },
-    responseTest: (state, action: PayloadAction<any>) => {
-      state.testData = action.payload;
-      console.log(action.payload)
+    nextPage : (state,action : PayloadAction<any>)=>{
+      const totalPageNum  = Math.ceil(state.total/12);
+      if(state.page >= totalPageNum) return;
+      if(10*(state.blockNum+1)< (state.page+1) ){
+        state.blockNum+=1;
+      }
+      state.page +=1;
+    },
+    prevPage: (state, action: PayloadAction<any>) => {
+      if(state.page <= 1 ) return;
+      if((state.page-1) <= (10*state.blockNum) ){
+        state.blockNum-=1;
+      }
+      state.page -=1;
     },
   },
 });
 
-export const { getTotal,setPage, } = page.actions;
+export const { getTotal,setPage,nextPage,prevPage} = page.actions;
 export const totalData = (state: RootState) => state.page.total;
 export const pageNum = (state: RootState) => state.page.page;
-
+export const blockNum = (state:RootState) => state.page.blockNum;
 
 export default page.reducer;
