9ac4df241d8d0185e32c464d030518e78f083e02
Hwang Jimin <jim1286@khu.ac.kr>
2022-10-19 01:51:30 +0900

    Feat : adminPageSlice 추가
    클릭한 product id 관리

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import kakaoLoginSliceReducer from '../features/kakaoLogin/kakaoLoginSlice';
 import userReservationSliceReducer from '../features/userReservation/userReservationSlice';
+import adminPageSliceReducer from '../features/adminPage/adminPageSlice';
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     kakaoLogin: kakaoLoginSliceReducer,
     userReservation: userReservationSliceReducer,
+    adminPage: adminPageSliceReducer,
   },
 });
 
