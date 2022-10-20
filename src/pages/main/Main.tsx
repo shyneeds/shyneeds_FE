cbce47b7259f18b2cde84d6ca35f60c9c48282d7
Yupmac <hohobanjang@gmail.com>
2022-10-21 05:31:19 +0900

    feat: 기획전여행 영역 추가 + 주석처리

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -5,6 +5,7 @@ import { SideStorage } from '../../components/common/aside/watched/WatchedStorag
 import { RecommendedByGroup } from '../../components/mainPage/recommended/RecommendedByGroup';
 import { RecommendedByRegion } from '../../components/mainPage/recommended/RecommendedByRegion';
 import { RecommendedByTheme } from '../../components/mainPage/recommended/RecommendedByTheme';
+// import { RecommendedByExhibition } from '../../components/mainPage/recommended/RecommendedByExhibition ';
 import Review from '../../components/review/Review';
 import Footer from '../../components/common/footer/Footer';
 
@@ -14,10 +15,13 @@ export default function Main() {
       <Header />
       <MainBanner />
       <SideStorage />
+      {/* 디자인 결정에 따라 수정 예정 */}
       {/* <PersonalSection /> */}
       <RecommendedByGroup />
       <RecommendedByRegion />
       <RecommendedByTheme />
+      {/* // 데이터 부족으로 정상 출력 불가하여 일단 주석 처리합니다. */}
+      {/* <RecommendedByExhibition /> */}
       <Review />
       <Footer />
     </>
