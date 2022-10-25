525ae386c060beeeb4536b939b2a6e13a906c704
Yupmac <hohobanjang@gmail.com>
2022-10-25 16:23:51 +0900

    작업 편의성을 위한 주석 처리

--- src/components/common/aside/watched/WatchedStorage.tsx
+++ src/components/common/aside/watched/WatchedStorage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '../../../../app/hooks';
 import { useAppDispatch } from '../../../../app/store';
@@ -5,14 +6,48 @@ import { productId } from '../../../../features/main/productSlice';
 
 export const WatchedStorage = () => {
   const dispatch = useAppDispatch();
-  const id = useAppSelector(productId);
-  localStorage.setItem('WATCHED_PRODUCTS', JSON.stringify(id));
+
+  // const test: string | null = localStorage.getItem('WATCHED_PRODUCTS');
+  // const testArr: any = useRef<string[]>([]);
+  // useEffect((): void => {
+  //   if (!testArr.current.includes(test)) {
+  //     if (testArr.current.length === 2) {
+  //       testArr.current.shift();
+  //     }
+  //     testArr.current.push(test);
+  //   } else {
+  //     testArr.current = testArr.current.filter((el: any) => {
+  //       return el !== test;
+  //     });
+  //     testArr.current.push(test);
+  //   }
+  // }, [test]);
+
+  // console.log(test);
+  // console.log(testArr);
+  // console.log(testArr.current);
+
+  // useEffect(() => {
+  //   const id = useAppSelector(productId);
+  //   let get_local: any = localStorage.getItem('WATCHED_PRODUCTS');
+
+  //   if (get_local == null) {
+  //     get_local = [];
+  //   } else {
+  //     get_local = JSON.parse(get_local);
+  //   }
+
+  //   get_local.push(id);
+  //   get_local = new Set(get_local);
+  //   get_local = [...get_local];
+  //   localStorage.setItem('WATCHED_PRODUCTS', JSON.stringify(get_local));
+  // }, []);
 
   return (
     <Container>
       <TextWrapper>
         <span>최근 본 상품</span>
-        <Quantity>({id.length})</Quantity>
+        {/* <Quantity>({localStorage.WATCHED_PRODUCTS.length})</Quantity> */}
       </TextWrapper>
       <StorageWrapper>
         <EmptyDiv>
