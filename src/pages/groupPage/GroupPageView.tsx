67bed4a0615998f92289db484a3c74e3b0d9eaa4
Yupmac <hohobanjang@gmail.com>
2022-10-28 16:39:58 +0900

    feat: 그룹페이지, 서브페이지에 공통 영역 추가(헤더, 푸터, 어사이드)

--- src/pages/groupPage/GroupPageView.tsx
+++ src/pages/groupPage/GroupPageView.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
 import GroupPage from '../../components/subPage/GroupPage';
+import Header from '../../components/common/header/Header';
+import Footer from '../../components/common/footer/Footer';
+import { AsideSection } from '../../components/common/aside/AsideSection';
 
 const GroupPageView = () => {
   return (
-    <GroupPage/>
-  )
-}
+    <>
+      <Header />
+      <GroupPage />
+      <AsideSection />
+      <Footer />
+    </>
+  );
+};
 
-export default GroupPageView
+export default GroupPageView;
