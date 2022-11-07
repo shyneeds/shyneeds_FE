ec91bd596bbe4ac83f7c9a2f7b4738820da20104
Youngpro95 <mmyoung95@naver.com>
2022-11-07 17:16:36 +0900

    Feat : customAxios 생성

---
+++ src/lib/customAxios.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from 'axios';
+
+const getCookie = function(name: string){
+  const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+  return value? value[2] : null;
+}
+
+export const customAxios: AxiosInstance = axios.create({
+  baseURL: `http://13.125.151.45:8080/api`, // 기본 서버 주소 입력
+  headers: {
+    Authorization: `Bearer ${getCookie('token')}`,
+  },
+});
